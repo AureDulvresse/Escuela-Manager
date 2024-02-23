f0afd25c7feb61f828bcc15aa8131b785978fcee
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-02-23 20:23:04 +0100

    Student API Views Done

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .models import Student
+from .serializers import StudentSerializer
+
+from .utils import Utils
 
 # Create your views here.
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def studentViews(request, pk = None) -> Response:
+
+    if request.method == "GET":
+
+        if not pk:
+            return Utils._list(Student, StudentSerializer)
+        
+        else:
+            return Utils._get(Student, StudentSerializer, pk)
+        
+    elif request.method == "POST":
+        data = request.data
+
+        new_student = Student.objects.create_user(
+            email = data['email'],
+            first_name = data['first_name'],
+            last_name = data['last_name'],
+            sexe = data['sexe'],
+            birthday = data['birthday'],
+            address = data['address'],
+            phone = data['phone'],
+            password = data['pwd'],
+        )
+        
+        serializer = StudentSerializer(new_student, many = False)
+
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        return Utils._update(request, Student, StudentSerializer, pk)
+    
+    elif request.method == "DELETE":
+        return Utils._delete(Student, pk)
+        
