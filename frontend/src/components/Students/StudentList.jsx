cc6ea75487bf7102b901c6484228303e93c7fce7
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-02-27 08:27:19 +0100

    Update StudentList.jsx

--- frontend/src/components/Students/StudentList.jsx
+++ frontend/src/components/Students/StudentList.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import ErrorRequest from "../ErrorRequest";
 import ListLoader from "../ListLoader";
 import { Link } from "react-router-dom";
+import { BiPencil, BiTrashAlt } from "react-icons/bi";
 
 const StudentList = () => {
   const queryKey = ["getStudent"];
@@ -27,17 +28,29 @@ const StudentList = () => {
       <h4 className="bg-indigo-600 text-white rounded-t-md w-full h-[50px] px-1 py-3 font-semibold">
         Liste des étudiants inscrits
       </h4>
-      <div className="flex flex-col w-full rounded-b-md">
+      <div className="flex flex-col w-full max-h-[240px] overflow-x-hidden rounded-b-md">
+        <div className="h-[40px] w-full border-t border-l border-r border-slate-200 py-2 px-1 flex items-center justify-between"></div>
         {listStudent.map((student, index) => {
           return (
             <Link
               to={student.uuid + "/show"}
-              className="h-[40px] w-full border-t border-l border-r border-slate-200 py-2 px-1 flex items-center"
+              className="h-[40px] w-full border-t border-l border-r border-slate-200 py-2 px-1 flex items-center justify-between hover:bg-slate-50"
               key={index}
             >
               <h5 className="text-[18px] leading-3 text-indigo-600 font-normal">
                 {student.first_name} {student.last_name}
               </h5>
+              <div className="flex justify-center gap-3">
+                <Link className="w-[40px] py-2 px-2 rounded-md text-indigo-600 hover:text-white border border-indigo-400 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 flex items-center justify-center">
+                  <BiPencil className="text-[18px] " />
+                </Link>
+                <Link
+                  to={student.uuid + "/remove"}
+                  className="w-[40px] px-2 py-2 rounded-md text-red-600 hover:text-white border border-red-500 shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500 flex items-center justify-center"
+                >
+                  <BiTrashAlt className="text-[18px] " />
+                </Link>
+              </div>
             </Link>
           );
         })}
