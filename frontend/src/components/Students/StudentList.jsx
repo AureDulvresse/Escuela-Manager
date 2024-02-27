e20258656a65fc2f8e66ef07ae848b397ce4ef98
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-02-27 09:53:10 +0100

    Table des étudiants implementer

--- frontend/src/components/Students/StudentList.jsx
+++ frontend/src/components/Students/StudentList.jsx
@@ -28,33 +28,63 @@ const StudentList = () => {
       <h4 className="bg-indigo-600 text-white rounded-t-md w-full h-[50px] px-1 py-3 font-semibold">
         Liste des étudiants inscrits
       </h4>
-      <div className="flex flex-col w-full max-h-[240px] overflow-x-hidden rounded-b-md">
-        <div className="h-[40px] w-full border-t border-l border-r border-slate-200 py-2 px-1 flex items-center justify-between"></div>
-        {listStudent.map((student, index) => {
-          return (
-            <Link
-              to={student.uuid + "/show"}
-              className="h-[40px] w-full border-t border-l border-r border-slate-200 py-2 px-1 flex items-center justify-between hover:bg-slate-50"
-              key={index}
-            >
-              <h5 className="text-[18px] leading-3 text-indigo-600 font-normal">
-                {student.first_name} {student.last_name}
-              </h5>
-              <div className="flex justify-center gap-3">
-                <Link className="w-[40px] py-2 px-2 rounded-md text-indigo-600 hover:text-white border border-indigo-400 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 flex items-center justify-center">
-                  <BiPencil className="text-[18px] " />
-                </Link>
-                <Link
-                  to={student.uuid + "/remove"}
-                  className="w-[40px] px-2 py-2 rounded-md text-red-600 hover:text-white border border-red-500 shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500 flex items-center justify-center"
-                >
-                  <BiTrashAlt className="text-[18px] " />
-                </Link>
-              </div>
-            </Link>
-          );
-        })}
-      </div>
+      <table className="w-full rounded-b-md">
+        <thead className="h-[40px] w-full">
+          <tr className="border-t border-l border-r border-slate-200 bg-slate-200 py-2 px-1">
+            <th className="text-[16px] font-light text-slate-700 text-center">
+              N°
+            </th>
+            <th className="text-[16px] font-light text-slate-7 text-center">
+              Nom & Prénom
+            </th>
+            <th className="text-[16px] font-light text-slate-7 text-center">
+              Sexe
+            </th>
+            <th className="text-[16px] font-light text-slate-7 text-center">
+              Promotion
+            </th>
+            <th className="text-[16px] font-light text-slate-7 text-center">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody className="w-full max-h-[250px] overflow-x-hidden">
+          {listStudent.map((student, index) => {
+            return (
+              <tr
+                className="border-t border-l border-r border-slate-200 py-3 px-1 hover:bg-slate-50"
+                key={index}
+              >
+                <td className="text-[18px] font-semibold text-center">
+                  {index + 1}
+                </td>
+                <td className="text-[18px] text-indigo-600 font-normal text-center">
+                  <Link to={student.uuid + "/show"}>
+                    {student.first_name} {student.last_name}
+                  </Link>
+                </td>
+                <td className="text-[18px] text-indigo-600 font-normal text-center">
+                  {student.sexe}
+                </td>
+                <td className="text-[18px] text-indigo-600 font-normal text-center">
+                  Terminal C2
+                </td>
+                <td className="flex justify-center gap-3 py-2">
+                  <Link className="w-[40px] py-2 px-2 rounded-md text-indigo-600 hover:text-white border border-indigo-400 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 flex items-center justify-center">
+                    <BiPencil className="text-[18px] " />
+                  </Link>
+                  <Link
+                    to={student.uuid + "/remove"}
+                    className="w-[40px] px-2 py-2 rounded-md text-red-600 hover:text-white border border-red-500 shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500 flex items-center justify-center"
+                  >
+                    <BiTrashAlt className="text-[18px] " />
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
